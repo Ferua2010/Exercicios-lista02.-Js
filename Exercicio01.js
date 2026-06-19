cad732acebc880cda2378289c601b4a3ba006552
Ferua2010 <ruabragafelipe@gmail.com>
2026-06-19 10:54:05 -0300

    Exercicio01.update2

--- Exercicio01.js
+++ Exercicio01.js
@@ -1,5 +1,16 @@
 //Sortear 20 numeros entre 0 e 10
+count = 0;
+divs = 0;
 for (i = 0; i < 21; i++) {
     sorteado = Math.floor(Math.random() * 10);
     console.log(`Sorteio numero ${i} Sorteado: ${sorteado}`);
+    if (sorteado > 5){
+        count = count + 1;
+    }
+    if (sorteado % 3 == 0) {
+        divs = divs + 1;
+    }
 }
+console.log(`NUmeros maiores que 5: ${count}`);
+console.log(`NUmeros divisivis por 3: ${divs}`);
+
