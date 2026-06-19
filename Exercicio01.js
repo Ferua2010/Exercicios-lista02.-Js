6c36c10679288f8915b9ee7f8f63b4b68a1bea53
Ferua2010 <ruabragafelipe@gmail.com>
2026-06-19 10:33:29 -0300

    Exercicio01.update.js

--- Exercicio01.js
+++ Exercicio01.js
@@ -1,1 +1,5 @@
-console.log("Olá Mundo")
+//Sortear 20 numeros entre 0 e 10
+for (i = 0; i < 21; i++) {
+    sorteado = Math.floor(Math.random() * 10);
+    console.log(`Sorteio numero ${i} Sorteado: ${sorteado}`);
+}
